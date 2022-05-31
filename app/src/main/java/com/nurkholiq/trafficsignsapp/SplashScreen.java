98857e49621fb1d6c72b13fa619959b4408db42f
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-05-31 13:03:01 +0700

    implement layout SplashScreen and MainSource

---
+++ app/src/main/java/com/nurkholiq/trafficsignsapp/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.nurkholiq.trafficsignsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
